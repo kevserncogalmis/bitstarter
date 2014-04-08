e6adb6a3c418d9fbc74de55dc4ace3b1b8e89518
nurcogalmis <kevserncogalmis@gmail.com>
2014-04-08 16:33:25 +0300

    Updated web.js and index.html

--- web.js
+++ web.js
@@ -6,6 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
         response.writeHead(200, {'Content-Type': 'html'});
         response.send(index);
+	response.end(index);
 });
 
 var port = process.env.PORT || 5000;
