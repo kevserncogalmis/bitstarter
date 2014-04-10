a4f7d3df854d62c83bd0bceed5af704f91494f53
nurcogalmis <kevserncogalmis@gmail.com>
2014-04-10 12:13:10 +0300

    Updated web.js and index.html

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-        response.sendfile((__dirname + '/index.html');
+        response.writeHead(200, {'Content-Type': 'html'});
+        response.send(index);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-
