9bf2623ab2f302938cbf3c840787adf794b7cb39
nurcogalmis <kevserncogalmis@gmail.com>
2014-04-10 12:08:51 +0300

    Updated web.js and index.html

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
-var app = express();
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
+var app = express.createServer(express.logger());
 
-app.get('/', function(req, res){
-      //response.send('Hello World 2!');
-      res.sendfile(__dirname + '/index.html');
+app.get('/', function(request, response) {
+        response.sendfile(index);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
