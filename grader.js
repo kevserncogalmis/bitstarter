a5bcadff877bcf0658adcd861e3a8466fd2037cc
nurcogalmis <kevserncogalmis@gmail.com>
2014-04-10 13:42:46 +0300

    Added check.json and grader.js and web.js

--- grader.js
+++ grader.js
@@ -20,16 +20,12 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
-
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var util = require('util');
-var rest = require('restler');
-
-
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,30 +61,18 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
-var loadURL = function() {
-    
-};
-
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-u, --url <url>', 'URL to file')
         .parse(process.argv);
-   
-    if (program.url) {
-      rest.get(program.url).on('complete', function(data) {
-        fs.writeFileSync('file.html', data);
-        var checkJson = checkHtmlFile('file.html', program.checks);
-        var outJson = JSON.stringify(checkJson, null, 4);
-        console.log(outJson);
-      });
-    } else {
-      var checkJson = checkHtmlFile(program.file, program.checks);
-      var outJson = JSON.stringify(checkJson, null, 4);
-      console.log(outJson);
-    }
-
+        if(program.url){
+        rest.get(apiurl).on('complete', response2console);
+      }
+    var checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
