cbb49acbccdb73b2ee2f7ffd31efe6d38a365127
kevserncogalmis <@gmail.com>
2014-05-14 10:56:14 +0000

    Edited index

--- web.js
+++ web.js
@@ -4,7 +4,6 @@ var app = express();
 app.get('/', function(reques, response){
       response.sendfile(__dirname + '/index3.html');
 });
-
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
